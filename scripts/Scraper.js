ab55034cb5feb68e5065545bb552a8782742c003
Phil Patterson <phil.patterson@brooksrunning.com>
2020-12-08 21:35:47 -0800

    request is deprecated, removing rp package and adding puppeteer

--- scripts/Scraper.js
+++ scripts/Scraper.js
@@ -1,10 +1,24 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
 class Scraper {
 
-    scrape(url) {
-        return rp(url)
+    // returns a response
+    async scrape(url) {
+
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+
+        // add all necessary selectors
+        // perhaps via json so its this file doesnt need to change
+        await page.waitForSelector('.afp-bottomLine-text')
+
+
+        // todo should parse be added here??
+        // need to figure out more methods with puppeteer.Page class
+        await page.screenshot({path: 'example.png'});
+        await browser.close();
+        console.log(page);
     }
 
     handleError(err) {
